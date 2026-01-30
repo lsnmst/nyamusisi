ad0fb0e8e4940fef775c5868374d3d7b9a9fb417
lsnmst <musetta.alessandro@gmail.com>
2026-01-30 18:09:46 +0100

    initial clean commit

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+  preprocess: vitePreprocess(),
+
+  compilerOptions: {
+    compatibility: {
+      componentApi: 4
+    }
+  }
+};
